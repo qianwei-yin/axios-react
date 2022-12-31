d48fb7fb022dfa62c7b1925c0697eae9e3ec7d26
Conway <937700753@qq.com>
2022-12-31 12:08:27 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup from "./examples/3-post-request";
+
 function App() {
-  return (
-    <main>
-      <Title />
-    </main>
-  );
+	return (
+		<main>
+			<Title />
+			<Setup />
+		</main>
+	);
 }
 
 export default App;
--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = "https://course-api.com/react-store-products";
 
 const FirstRequest = () => {
-  useEffect(() => {
-    console.log('first axios request');
-  }, []);
+	async function fetchData() {
+		try {
+			const response = await axios.get(url);
+			const data = response.data;
+			console.log(data);
+		} catch (error) {
+			// using axios, all the errors are caught in the catch-block. But using fetch, it only catches network error.
+			console.log(error.response);
+		}
+	}
 
-  return <h2 className='text-center'>first request</h2>;
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
